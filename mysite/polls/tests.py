614ae4e7447fac7fb402498e82548bfb6cb5ecc4
avimehenwal <avi.mehanwal@gmail.com>
2021-12-04 18:32:31 +0100

    test(mysite): :sparkles: add json api tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from django.test import Client
+
+from .models import Question
+
+"""
+python manage.py test polls -v 2
+"""
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_if_new_record_could_be_created(self):
+        record = Question(question_text="sample text", pub_date=timezone.now())
+        record.save()
+
+        client = Client()
+        response = client.get("/polls/question/1/")
+        print(response)
+
+        self.assertEqual(
+            response.json()["status"],
+            "Success"
+            # Question.objects.filter(id=1).first(),
+        )
