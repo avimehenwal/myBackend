78ef9169c3ec8d2f2d0091c4da11370caf71cacf
avimehenwal <avi.mehanwal@gmail.com>
2021-12-04 18:45:42 +0100

    feat(mysite): :lipstick: customize admin panel view, expand foreign keys

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from django.db.models.enums import Choices
+from .models import Choice, Question
 
-from .models import Question
-from .models import Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
